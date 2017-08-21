b3b8e5145b9fe011a60400ce76e63e19602b9100
jaredburks <jarabu93@gmail.com>
2017-08-21 11:34:01 -0500

    added problem 5 to reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -7,10 +7,24 @@ function getUserInput(){
 	return prompt(stuff);
 }
 
-function myFunction(){
+function reverseInput(){
 	alert("Your input reversed is " + getUserInput().split("").reverse().join(""));
 }
 
-myFunction();
+reverseInput();
 //console.log("String: " + prompt(stuff, "Type Here"));
 
+//Below is problem 5, palindrome checker
+var someText = prompt("Type something: ");
+someText = someText.replace(/\s+/g, '');
+
+function palindrome(){
+	if(someText === someText.split("").reverse().join("")){
+		console.log(someText);
+		console.log("Its a palindrome thingy!");
+	}	
+	else{
+		console.log("Not a palindrome; Lame!");
+	}	
+}
+palindrome();
